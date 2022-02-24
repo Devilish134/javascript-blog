4291d4e168265ced04ae44edb143db0e18155318
Kamil Musialik <kamilmusialik22@gmail.com>
2022-02-24 14:00:00 +0100

    add add active

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 'use strict';
 
 function titleClickHandler(){
+    const clickedElement = this;
     console.log(event);
 
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
-    }
+        activeLink.classList.remove('active');
+        }
 
     /* add class 'active' to the clicked link */
+    for(let activeLink of activeLinks){
+        activeLink.classList.add('active');
+        }
 
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .post.active');
@@ -24,10 +28,10 @@ function titleClickHandler(){
     /* find the correct article using the selector (value of 'href' attribute) */
 
     /* add class 'active' to the correct article */
-  }
+}
   
   const links = document.querySelectorAll('.titles a');
   
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
-}
+    }
