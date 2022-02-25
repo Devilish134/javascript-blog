f74ca670addfccac736370f7d16d5d68771c9776
Kamil Musialik <kamilmusialik22@gmail.com>
2022-02-25 11:36:53 +0100

    add finding articles and showung them

--- js/script.js
+++ js/script.js
@@ -1,33 +1,37 @@
 'use strict';
 
-function titleClickHandler(){
+function titleClickHandler(event){
+    event.preventDefault();
     const clickedElement = this;
-    console.log(event);
+    console.log('Link was clicked!');
 
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
-        }
+    }
 
     /* add class 'active' to the clicked link */
-    for(let activeLink of activeLinks){
-        activeLink.classList.add('active');
-        }
+    clickedElement.classList.add('active');
 
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .post.active');
 
     for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
+        activeArticle.classList.remove('active');
     }
 
     /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(this)
 
     /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
   
   const links = document.querySelectorAll('.titles a');
